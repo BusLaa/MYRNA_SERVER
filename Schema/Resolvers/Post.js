cbef0a63235df2b7494f52c42b3e851ab60ac2c7
busliksuslik <nbuslaev35@gmail.com>
2023-04-10 15:01:01 +0300

    deleteComment

--- Schema/Resolvers/Post.js
+++ Schema/Resolvers/Post.js
@@ -147,7 +147,7 @@ const PostResolvers = {
         },
         deleteComment: async (_, {commentId}, ctx) =>{
             const user = verify(ctx.req.headers['verify-token'], process.env.SECRET_WORD).user;
-            const user_id = (await models.Post.findOne({where: {id: commentId}})).Author
+            const user_id = (await models.Comment.findOne({where: {id: commentId}})).Author
             if (!isRolesInUser(await getUserRoles(user.id), ["ADMIN"]) && user.id != user_id) throw Error("You do not have rights (basically woman)")
 
             models.Comment.update({deleted: true}, {where: {id: postId}})
