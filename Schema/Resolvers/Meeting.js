3ce59f6a1cee4378206b2929cf0c23ade712a962
busliksuslik <nbuslaev35@gmail.com>
2023-04-17 11:23:34 +0300

    id s

--- Schema/Resolvers/Meeting.js
+++ Schema/Resolvers/Meeting.js
@@ -207,13 +207,13 @@ const MeetingResolvers = {
             return (await models.MeetingType.findOne({where:{id : meetingObj.typeId}})).name;
         },
         members: async (meeting) => {
-            return models.UserMeeting.findAll({where: {meetingId: meeting.id}})
+            return (await models.UserMeeting.findAll({where: {meetingId: meeting.id}})).map((a) => {id: a})
         },
         creator: async (meeting) => {
             return {id : (await models.Meeting.findOne({where: {id: meeting.id}})).creator}
         },
         places: async (meeting) => {
-            return models.PlaceMeetings.findAll({where: {MeetingId: meeting.id}})
+            return (await models.PlaceMeetings.findAll({where: {MeetingId: meeting.id}})).map((a) => {id: a})
         },
         chief: async (meeting) =>{
             return {id :(await models.Meeting.findOne({where: {id: meeting.id}})).chief}
