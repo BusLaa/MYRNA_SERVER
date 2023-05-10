11afc39da5cfe66f381220c7355d0b8db9716cea
busliksuslik <nbuslaev35@gmail.com>
2023-05-10 10:41:19 +0300

    addPostToresolver Issue

--- Schema/Resolvers/Corner.js
+++ Schema/Resolvers/Corner.js
@@ -49,7 +49,7 @@ const CornerResolvers = {
             const user = verify(ctx.req.headers['verify-token'], process.env.SECRET_WORD).user;
             if (!isRolesInUser(await getUserRoles(user.id), ["ADMIN"]) && user.id !== userId ) throw Error("You do not have rights")
 
-            const cornerPost = models.CornerPost.create({
+            const cornerPost = await models.CornerPost.create({
                 userId: userId,
                 postId: postId
             })
