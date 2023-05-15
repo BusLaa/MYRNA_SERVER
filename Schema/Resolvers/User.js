e452392701fa09c6de2cbd487ea6253b47d9efb1
busliksuslik <nbuslaev35@gmail.com>
2023-05-15 12:19:50 +0300

    getUserByName excludeMeeting 4

--- Schema/Resolvers/User.js
+++ Schema/Resolvers/User.js
@@ -59,7 +59,7 @@ const UserResolvers = {
 
             let include = ""
             if (excludeMeeting){
-                where.push({[Op.not] : sequelize.literal (`Meetings.id = ${excludeMeeting}`)})
+                where.push({[Op.not] : sequelize.literal (`Meetings.id <> ${excludeMeeting}`)})
                 include = "Meetings"
 
             }
